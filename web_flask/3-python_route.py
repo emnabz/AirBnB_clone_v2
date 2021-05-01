4c6b1f6cee3648b2af2e818a01ef3749907fedb8
emna <1947@holbertonschool.com>
2021-05-01 01:55:17 +0100

    task3

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -27,7 +27,7 @@ def cisfun(text):
 @app.route('/python/<text>', strict_slashes=False)
 def pyiscool(text='is cool'):
     """display "Python" followed by the value of the text variable"""
-    return 'Python' + text.replace('_', ' ')
+    return 'Python ' + text.replace('_', ' ')
 
 
 if __name__ == '__main__':
