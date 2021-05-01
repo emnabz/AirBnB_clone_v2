67b2d85862c36c977ea54a0c93af2e383cdf6d85
emna <1947@holbertonschool.com>
2021-05-01 01:35:05 +0100

    task2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""starts a flask web app"""
+"""
+starts a Flask web application
+"""
 
 from flask import Flask
 app = Flask(__name__)
@@ -7,20 +9,20 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def index():
-    """displays Hello HBNB"""
+    """returns Hello HBNB!"""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """displays HBNB"""
+    """returns HBNB"""
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def ciscool(text):
+def cisfun(text):
     """display “C ” followed by the value of the text variable"""
-    return 'C' + text.replace('_', '')
+    return 'C ' + text.replace('_', ' ')
 
 
 if __name__ == '__main__':
